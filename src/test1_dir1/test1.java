cafb5aa7170473d5abe22450d6d78960182a09f8
jinye318 <jinye318@naver.com>
2015-03-19 16:47:13 +0900

    test1

---
+++ src/test1_dir1/test1.java
@@ -0,0 +1,11 @@
+package test1_dir1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It is test in dir1");
+		System.out.println("second try");
+	}
+
+}
